fc12cd031c8cc70424b4118edf363b1eff56db17
John Desktop <jhfv02@gmail.com>
2023-08-06 00:56:17 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,28 @@
+﻿using API_Clientes.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Clientes.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClienteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ClienteController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var listClientes = await _context.Clientes.ToListAsync();
+            return Ok(listClientes);
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Clientes.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
